f016f23d82eba2b88cc86f8f7eb91261126413f0
MarcosLehmkuhl <m.v.lehmkuhl@gmail.com>
2015-11-21 20:07:43 -0200

    idle

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 var game = new Phaser.Game(1024, 768, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
+var facing = 'right';
+
 function preload() {
   game.load.image('floor', './assets/images/floor.png');
   game.load.image('background', './assets/images/background.png');
-  game.load.spritesheet('hero', './assets/images/hero.png', 150, 175, 16);
+  game.load.spritesheet('hero', './assets/images/hero2.png', 150, 160, 15);
 }
 
 
@@ -11,19 +13,15 @@ function create() {
   game.world.setBounds(0, 0, 400, 400);
   game.add.sprite(0, 0, 'floor');
   game.add.sprite(0, background_position, 'background');
-  hero = game.add.sprite(0,535, 'hero');
+  hero = game.add.sprite(0,537, 'hero');
 
     //  Here we add a new animation called 'walk'
     //  Because we didn't give any other parameters it's going to make an animation from all available frames in the 'mummy' sprite sheet
-  hero.animations.add('left',[0,1,2,3,4,5,6,7]);
-  hero.animations.add('right',[8,9,10,11,12,13,14,15]);
+  hero.animations.add('walk',[0,1,2,3,4,5,6,7]);
+  hero.animations.add('idle',[8,9,10,11,12,13,14]);
   game.camera.follow(hero);
 
   cursors = game.input.keyboard.createCursorKeys();
-    //  And this starts the animation playing by using its key ("walk")
-    //  30 is the frame rate (30fps)
-    //  true means it will loop when it finishes
-
 }
 
 
@@ -31,18 +29,24 @@ function update() {
    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
     {
       hero.x -= 2;
-      hero.animations.play('left', 6, true);
+      hero.animations.play('walk', 10, true);
       game.camera.y -= 4
+      facing = 'left';
     }
     else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
     {
         hero.x += 2;
-        hero.animations.play('right', 6, true);
+        hero.animations.play('walk', 10, true);
         game.camera.y -= 4
+        facing = 'right';
     }
     else {
-       hero.animations.stop();
-       hero.frame = 8;
+       if(facing == 'left') {
+        hero.animations.play('idle', 10, true);
+       }
+       if(facing == 'right') {
+        hero.animations.play('idle', 10, true);
+       }
     }
 }
 
