34af9076f914ed4c4892dd8ef87a61e05e1554f6
MarcosLehmkuhl <m.v.lehmkuhl@gmail.com>
2015-11-21 22:18:32 -0200

    collide and jump

--- src/js/main.js
+++ src/js/main.js
@@ -2,23 +2,28 @@ var game = new Phaser.Game(1024, 768, Phaser.CANVAS, '', { preload: preload, cre
 var facing = 'right';
 
 function preload() {
-  game.load.image('floor', './assets/images/floor.png');
+  game.load.image('floor', './assets/images/floor.png', 68,150);
   game.load.image('background', './assets/images/background.png');
   game.load.spritesheet('hero', './assets/images/hero2.png', 150, 160, 15);
 }
 
 
 function create() {
-  background_position = 0;
-  game.world.setBounds(0, 0, 400, 400);
-  game.add.sprite(0, 0, 'floor');
-  game.add.sprite(0, background_position, 'background');
-  hero = game.add.sprite(0,537, 'hero');
-
-    //  Here we add a new animation called 'walk'
-    //  Because we didn't give any other parameters it's going to make an animation from all available frames in the 'mummy' sprite sheet
+  game.world.setBounds(0, 0, 1024, 695); //695
+  game.add.sprite(0, 0, 'background');
+ 
+  hero = game.add.sprite(0,400, 'hero');
+  game.physics.enable(hero, Phaser.Physics.ARCADE);
+  hero.body.bounce.y = 0;
+  hero.body.gravity.y = 500;
+  hero.body.collideWorldBounds = true;
   hero.animations.add('walk',[0,1,2,3,4,5,6,7]);
   hero.animations.add('idle',[8,9,10,11,12,13,14]);
+  
+  ground = game.add.sprite(0,0,'floor');
+  game.physics.enable(ground, Phaser.Physics.ARCADE);
+  ground.body.immovable = true;
+  
   game.camera.follow(hero);
 
   cursors = game.input.keyboard.createCursorKeys();
@@ -26,6 +31,7 @@ function create() {
 
 
 function update() {
+  game.physics.arcade.collide(hero, ground);
    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
     {
       hero.x -= 2;
@@ -40,6 +46,11 @@ function update() {
         game.camera.y -= 4
         facing = 'right';
     }
+     else if (game.input.keyboard.isDown(Phaser.Keyboard.UP) && hero.body.onFloor())
+    {
+      hero.animations.stop();
+      hero.body.velocity.y = -250;
+    }
     else {
        if(facing == 'left') {
         hero.animations.play('idle', 10, true);
@@ -50,6 +61,4 @@ function update() {
     }
 }
 
-function render() {
-  game.debug.cameraInfo(game.camera, 32, 32);
-}
+function render() {}
