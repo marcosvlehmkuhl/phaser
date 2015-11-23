47b55ad6e1b9c28fd5ed19869026117d718fba96
MarcosLehmkuhl <m.v.lehmkuhl@gmail.com>
2015-11-23 21:56:22 -0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,53 +1,84 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var game = new Phaser.Game(1024, 768, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1024, 768, Phaser.CANVAS, '', { preload: preload, create: create, update: update, render:render });
+var facing = 'right';
+
 function preload() {
-  game.load.image('floor', './assets/images/floor.png');
+  game.load.image('floor', './assets/images/floor.png', 68,150);
   game.load.image('background', './assets/images/background.png');
-  game.load.spritesheet('hero', './assets/images/hero.png', 150, 175, 16);
+  game.load.spritesheet('hero', './assets/images/hero2.png', 150, 160, 34);
 }
 
 
 function create() {
-  background_position = 0;
-  game.world.setBounds(0, 0, 400, 400);
-  game.add.sprite(0, 0, 'floor');
-  game.add.sprite(0, background_position, 'background');
-  hero = game.add.sprite(0,535, 'hero');
-
-    //  Here we add a new animation called 'walk'
-    //  Because we didn't give any other parameters it's going to make an animation from all available frames in the 'mummy' sprite sheet
-  hero.animations.add('left',[0,1,2,3,4,5,6,7]);
-  hero.animations.add('right',[8,9,10,11,12,13,14,15]);
+  bg = game.add.sprite(0, 0, 'background');
+ 
+  hero = game.add.sprite(0,400, 'hero');
+  game.physics.enable(hero, Phaser.Physics.ARCADE);
+  hero.body.bounce.y = 0;
+  hero.body.gravity.y = 500;
+  hero.body.collideWorldBounds = true;
+  hero.anchor.setTo(0.5, 0.5);
+  hero.animations.add('walk',[0,1,2,3,4,5,6,7]);
+  hero.animations.add('idle',[8,9,10,11,12,13,14]);
+  hero.animations.add('jump',[16,17,18,19,20,21,22,23,24,25,26,27,28,29]);
+  hero.animations.add('punch',[30,31]);
+  hero.animations.add('kick',[32,33]);
+  
+  ground = game.add.sprite(0,0,'floor');
+  game.physics.enable(ground, Phaser.Physics.ARCADE);
+  ground.body.immovable = true;
+  game.camera.bounds.width = ground.body.width;
+  game.world.setBounds(0, 0, ground.body.width, 695); //695
   game.camera.follow(hero);
 
   cursors = game.input.keyboard.createCursorKeys();
-    //  And this starts the animation playing by using its key ("walk")
-    //  30 is the frame rate (30fps)
-    //  true means it will loop when it finishes
-
 }
 
 
 function update() {
+  game.physics.arcade.collide(hero, ground);
    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
     {
-      hero.x -= 2;
-      hero.animations.play('left', 6, true);
-      game.camera.y -= 4
+      hero.x -= 4;
+      hero.animations.play('walk', 10);
+      game.camera.x -= 4;
+      facing = 'left';
     }
     else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
     {
-        hero.x += 2;
-        hero.animations.play('right', 6, true);
-        game.camera.y -= 4
+      hero.x += 4;
+      facing = 'right';
+      if(hero.body.onFloor() == false) {
+
+      }
+      else {
+        hero.animations.play('walk', 10);
+      }
+    }
+     else if (game.input.keyboard.isDown(Phaser.Keyboard.UP) && hero.body.onFloor())
+    {
+      hero.animations.play('jump', 12, true);
+      hero.body.velocity.y = -250;
+    }
+    else if(game.input.keyboard.isDown(Phaser.Keyboard.Z)) {
+      hero.animations.play('punch',12,false);
+    }
+     else if(game.input.keyboard.isDown(Phaser.Keyboard.X)) {
+      hero.animations.play('kick',6,false);
+    }
+     else if(hero.body.onFloor() == false) {
     }
     else {
-       hero.animations.stop();
-       hero.frame = 8;
+       if(facing == 'left') {
+        hero.animations.play('idle', 10, true);
+       }
+       if(facing == 'right') {
+        hero.animations.play('idle', 10, true);
+       }
     }
 }
 
 function render() {
-  game.debug.cameraInfo(game.camera, 32, 32);
+  game.debug.cameraInfo(game.camera,32,32);
 }
 },{}]},{},[1])
