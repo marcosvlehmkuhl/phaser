461ace6f77956f09bebc9eec2688ba90f9238c83
MarcosLehmkuhl <m.v.lehmkuhl@gmail.com>
2015-11-25 22:16:27 -0200

    collision

--- src/js/main2.js
+++ src/js/main2.js
@@ -10,33 +10,39 @@ function preload() {
 
 
 function create() {
+
+  // game.physics.startSystem(Phaser.Physics.ARCADE);
+
   bg = game.add.sprite(0, 0, 'background');
- 
+
+  ground = game.add.sprite(0,0,'floor');
+  game.physics.enable(ground, Phaser.Physics.ARCADE);
+  ground.body.immovable = true;
+  // ground.body.setSize(ground.body.width,68);
+
   enemy = game.add.sprite(200, 530, 'enemy1');
   game.physics.enable(enemy, Phaser.Physics.ARCADE);
+  enemy.body.bounce.y = 0;
+  enemy.body.gravity.y = 500;
   enemy.body.collideWorldBounds = true;
-  enemy.animations.add('walk', [7,6,5,4,3,2,1,0]); 
+  enemy.animations.add('walk', [7,6,5,4,3,2,1,0]);
 
   hero = game.add.sprite(0,400, 'hero');
   game.physics.enable(hero, Phaser.Physics.ARCADE);
   hero.body.bounce.y = 0;
   hero.body.gravity.y = 500;
   hero.body.collideWorldBounds = true;
-  hero.anchor.setTo(0.5, 0.5);
   hero.animations.add('walk',[0,1,2,3,4,5,6,7]);
   hero.animations.add('idle',[8,9,10,11,12,13,14]);
   hero.animations.add('jump',[16,17,18,19,20,21,22,23,24,25,26,27,28,29]);
   hero.animations.add('punch',[30,31]);
   hero.animations.add('kick',[32,33]);
+  
 
-  ground = game.add.sprite(0,0,'floor');
-  game.physics.enable(ground, Phaser.Physics.ARCADE);
-  ground.body.immovable = true;
   game.camera.bounds.width = ground.body.width;
-  game.world.setBounds(0, 0, ground.body.width, 695); //695
+  game.world.setBounds(0, 0, ground.body.width, 768); //695
   game.camera.follow(hero);
 
-  cursors = game.input.keyboard.createCursorKeys();
 }
 
 function heroCollider(player,enemy) {
@@ -45,24 +51,26 @@ function heroCollider(player,enemy) {
 
 function update() {
   game.physics.arcade.collide(hero, ground);
-  game.physics.arcade.collide(hero, enemy);
-  game.physics.arcade.overlap(hero, enemy, heroCollider);
-  enemy.animations.play('walk',10,true);
-  enemy.animations.stop();
+  // game.physics.arcade.collide(hero, enemy);
+  // game.physics.arcade.overlap(hero, enemy, heroCollider);
+  hero.body.velocity.x = 0;
+  // enemy.body.velocity.x = 0;
   enemy.frame = 7;
    if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT))
     {
       hero.x -= 4;
+      // hero.body.velocity.x = -150;
       hero.animations.play('walk', 10);
       game.camera.x -= 4;
       facing = 'left';
     }
-    else if (game.physics.arcade.distanceBetween < 100) {
-      console.log('uhul');
+    else if (game.physics.arcade.distanceBetween(hero, enemy) < 50) {
+      console.log('distanceBetween');
     }
     else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
     {
       hero.x += 4;
+      // hero.body.velocity.x = +150;
       facing = 'right';
       if(hero.body.onFloor() == false) {
 
